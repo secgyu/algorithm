e70c7ca6a119e80d8efe330fe125bd7b0643a9ae
gyuminKim-kr <abc2232002@gmail.com>
2023-11-19 16:00:44 +0900

    gcd 제거

--- gcd.py
+++ gcd.py
@@ -1,12 +1,1 @@
-V = int(input())
-E = int(input())
-g = []
-for i in range(V):
-    g.append([0]*V)
-for i in range(E):
-    u,v = input().split()
-    u,v = int(u), int(v)
-    g[u][v] = 1
-    g[v][u] = 1
-print(g)
-
+a
